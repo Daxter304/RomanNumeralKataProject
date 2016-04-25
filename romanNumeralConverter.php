8b9595d787dbc878de4c1ad709e3bc5b2b825eb3
Sean Baluha <daxter304@yahoo.com>
2016-04-25 16:31:11 -0400

    Converter can now do reversed numerals like 9-IX

--- romanNumeralConverter.php
+++ romanNumeralConverter.php
@@ -1,37 +1,62 @@
 <?php
-// $rnc = new romanNumeralConverter();
-// $numToConvert = readline("Enter an Arabic number: ");
-// $romanNumeral = $rnc->convertToRoman($numToConvert);
-// echo 'Roman Numeral: ' . $romanNumber;
 
 class romanNumeralConverter {
   private $romanNumerals = [
-    1000 => 'M',
-    500 => 'D',
-    100 => 'C',
-    50 => 'L',
-    10 => 'X',
-    5 => 'V',
-    1 => 'I'
+    'M' => 1000,
+    'D' => 500,
+    'C' => 100,
+    'L' => 50,
+    'X' => 10,
+    'V' => 5,
+    'I' => 1
   ];
 
-  public function __construct() {
-  }
+  private $romanPosition = [
+      'M',
+      'D',
+      'C',
+      'L',
+      'X',
+      'V',
+      'I'
+  ];
 
   public function convertToRoman($arabicNumber) {
-    $romanNumeral = "";
+    $romanNumeralsString = "";
 
     while ($arabicNumber > 0) {
       foreach ($this->romanNumerals as $key => $value) {
-        if ($arabicNumber >= $key) {
-          $arabicNumber = $arabicNumber - $key;
-          $romanNumeral .= $value;
+        if ($arabicNumber >= $value) {
+          $arabicNumber -= $value;
+          $romanNumeralsString .= $key;
           break;
+        }
+      }
+
+      if (strlen($romanNumeralsString) >= 4) {
+        $lastFourNumerals = str_split(substr($romanNumeralsString, -4, 4));
+
+        $oneLess = true;
+        foreach ($lastFourNumerals as $value) {
+          if ($value !== $lastFourNumerals[0]) {
+            $oneLess = false;
+            break;
+          }
+        }
+
+        if ($oneLess === true) {
+          $firstNumeralInInstance = substr($romanNumeralsString, -5, 1);
+          $pos = array_search($firstNumeralInInstance, $this->romanPosition);
+
+          $numeralsToReplace = substr($romanNumeralsString, -5, 5);
+          $numeralsToAdd = $lastFourNumerals[0] . $this->romanPosition[$pos-1];
+
+          $romanNumeralsString = str_replace($numeralsToReplace, $numeralsToAdd, $romanNumeralsString);
         }
       }
     }
 
-    return $romanNumeral;
+    return $romanNumeralsString;
   }
 }
 ?>
